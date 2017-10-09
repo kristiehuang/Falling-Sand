e1553bee577aced5484e870420e293372aa7d1d4
Kristie Huang <kristiehuang01@gmail.com>
2017-10-09 11:01:38 -0700

    Initial commit 2.0

---
+++ SandLab.java
@@ -0,0 +1,61 @@
+import java.awt.*;
+import java.util.*;
+
+public class SandLab
+{
+  public static void main(String[] args)
+  {
+    SandLab lab = new SandLab(120, 80);
+    lab.run();
+  }
+  
+  
+  //add constants for particle types here
+  public static final int EMPTY = 0;
+  public static final int METAL = 1;
+  
+  //do not add any more fields
+  private int[][] grid;
+  private SandDisplay display;
+  
+  public SandLab(int numRows, int numCols)
+  {
+    String[] names;
+    names = new String[2];
+    names[EMPTY] = "Empty";
+    names[METAL] = "Metal";
+    display = new SandDisplay("Falling Sand", numRows, numCols, names);
+  }
+  
+  //called when the user clicks on a location using the given tool
+  private void locationClicked(int row, int col, int tool)
+  {
+  }
+
+  //copies each element of grid into the display
+  public void updateDisplay()
+  {
+  }
+
+  //called repeatedly.
+  //causes one random particle to maybe do something.
+  public void step()
+  {
+  }
+  
+  //do not modify
+  public void run()
+  {
+    while (true)
+    {
+      for (int i = 0; i < display.getSpeed(); i++)
+        step();
+      updateDisplay();
+      display.repaint();
+      display.pause(1);  //wait for redrawing and for mouse
+      int[] mouseLoc = display.getMouseLocation();
+      if (mouseLoc != null)  //test if mouse clicked
+        locationClicked(mouseLoc[0], mouseLoc[1], display.getTool());
+    }
+  }
+}
